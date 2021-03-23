cf613050c0b972c27863fea1c41c1b7c8f32dec4
tg-z <parasitism@protonmail.com>
2021-03-22 20:56:37 -0700

    tweaks to feedgen.py

--- build/feedgen.py
+++ build/feedgen.py
@@ -3,13 +3,14 @@
 # from feedgen import FeedGenerator
 import feedgen as feed
 fg = feed.FeedGenerator()
-fe = fg.add_entry()
+# fe = fg.add_entry()
+entry = feedgen.add_entry()
 atomfeed = fg.atom_str(pretty=True) # Get the ATOM feed as string
 rssfeed  = fg.rss_str(pretty=True) # Get the RSS feed as string
 fg.atom_file('atom.xml') # Write the ATOM feed to a file
 fg.rss_file('rss.xml') # Write the RSS feed to a file
 fg.id('http://lernfunk.de/media/654321')
-fg.title('Some Testfeed')
+fg.title('til')
 fg.author( {'name':'John Doe','email':'john@example.de'} )
 fg.link( href='http://example.com', rel='alternate' )
 fg.logo('http://ex.com/logo.jpg')
